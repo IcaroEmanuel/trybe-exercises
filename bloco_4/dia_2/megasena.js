bf08eadfac900358750cf28de1704b3f832e0ccd
icaroemanuel <icaro.emanuel@outlook.com>
2021-06-24 17:32:41 -0300

    Criaçao do arquivo megasena.js

---
+++ bloco_4/dia_2/megasena.js
@@ -0,0 +1,9 @@
+//Fazer um algoritmo que faça o sorteio da megasena
+
+//1 - Criar uma estrutura de números escolhidos pela pessoa que está jogando
+
+//2 - Criar uma estrutura de números aleatórios de 1 a 60
+
+/*Comparar os números escolhidos pelos números sorteados 
+  (Dica use um for para percorrer a estrutura de números aleatórios e outro for dentro dele para percorrer a estrutura de números escolhidos)
+*/
