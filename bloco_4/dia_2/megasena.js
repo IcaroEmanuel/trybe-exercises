4451861082ea3560f7d51c362c8a68ff3f5110b2
icaroemanuel <icaro.emanuel@outlook.com>
2021-06-24 19:13:02 -0300

    Feito a estrutura de numeros escolhidos e numeros sorteados

--- bloco_4/dia_2/megasena.js
+++ bloco_4/dia_2/megasena.js
@@ -1,9 +1,15 @@
-//Fazer um algoritmo que faça o sorteio da megasena
+//Criar um algoritmo que simule o sorteio da megasena
 
 //1 - Criar uma estrutura de números escolhidos pela pessoa que está jogando
+let numerosEscolhidos = [19, 23, 13, 5, 25, 24]
 
-//2 - Criar uma estrutura de números aleatórios de 1 a 60
+//2 - Criar uma estrutura de 6 números aleatórios de 1 a 60
+let numerosSorteados = []
 
-/*Comparar os números escolhidos pelos números sorteados 
-  (Dica use um for para percorrer a estrutura de números aleatórios e outro for dentro dele para percorrer a estrutura de números escolhidos)
-*/
+for (let contador = 0; contador < 6; contador += 1){
+  let numeroAleatorio = Math.floor(Math.random() * 60) + 1
+  numerosSorteados.push(numeroAleatorio)
+}
+
+console.log(numerosSorteados)
+//3 - Comparar os números escolhidos com os números sorteados e verificar se há algum número igual 
