2a217a16ff9fa992f03d9741bf7fe9a2cdb619b1
icaroemanuel <icaro.emanuel@outlook.com>
2021-10-01 09:43:14 -0300

    Refatoração - a função authorBornIn1947 passa a ter retorno implícito

--- fundamentos/bloco-8-hofs/dia-8-2/search-author-born-1947.js
+++ fundamentos/bloco-8-hofs/dia-8-2/search-author-born-1947.js
@@ -2,9 +2,7 @@ const assert = require('assert');
 const { books } = require('./data');
 
 // Encontre o nome da primeira pessoa autora do livro nascida no ano de 1947
-const authorBornIn1947 = () => {
-  const nameAuthor = books.find((book) => book.author.birthYear === 1947);
-  return nameAuthor.author.name;
-}
+const authorBornIn1947 = () => books.find((book) => 
+  book.author.birthYear === 1947).author.name;
 
 assert.strictEqual(authorBornIn1947(), 'Stephen King');
