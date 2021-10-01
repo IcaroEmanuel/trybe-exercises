43c01e24bf25e9142c33c3162667a1f7c01629f2
icaroemanuel <icaro.emanuel@outlook.com>
2021-09-30 21:55:21 -0300

    Criada uma função que encontra o primeiro autor nascido em 1947

--- fundamentos/bloco-8-hofs/dia-8-2/search-author-born-1947.js
+++ fundamentos/bloco-8-hofs/dia-8-2/search-author-born-1947.js
@@ -1,1 +1,9 @@
-const data = require('./data');
+const { books } = require('./data');
+
+// Encontre o nome da primeira pessoa autora do livro nascida no ano de 1947
+const authorBornIn1974 = () => {
+  return books.find((book) => book.author.birthYear === 1947);
+}
+
+const author = authorBornIn1974()
+console.log(author);
