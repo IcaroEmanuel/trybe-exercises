e7eb0591d41e66140f2c4e9675b6b33c27ff4ea8
icaroemanuel <icaro.emanuel@outlook.com>
2021-10-13 13:24:18 -0300

    Criação de um mecanismo de pesquisa da maior nota de todas pessoas estudantes

--- fundamentos/bloco-8-hofs/dia-8-3/students.js
+++ fundamentos/bloco-8-hofs/dia-8-3/students.js
@@ -11,4 +11,39 @@ const returnAllStudentsByTurn = (students, turn) => {
   return returnNameAndLastname(studentsByTurn);
 }
 
-console.log(returnAllStudentsByTurn(students, 'Tarde'));
+// Encontra uma pessoa estudante pelo nome e sobrenome
+const findStudentByNameAndLastname = (listOfStudents, nameOfStudent, lastNameOfStudent) => 
+  listOfStudents.find((studant) => 
+    studant.nome === nameOfStudent && 
+    studant.sobrenome === lastNameOfStudent);
+
+// Verificar qual a maior nota de uma pessoa estudante
+const checkTheHighestScore = (listOfStudents, nameOfStudent, lastNameOfStudent) => {
+  let bigger = {rate: 0, matter: ''};
+  const studentInfo = findStudentByNameAndLastname(listOfStudents, nameOfStudent, lastNameOfStudent);
+  const matters = studentInfo.materias.map((matter) => {
+    const objMatters = {
+      notas: matter.nota,
+      materias: matter.name,
+    };
+    return objMatters;
+  });
+  matters.forEach(score => {
+    if (score.notas >= bigger.rate) {
+      bigger.rate = score.notas;
+      bigger.matter = score.materias;
+    }
+  });
+  return `Maior nota de ${nameOfStudent} ${lastNameOfStudent} = ${bigger.matter} : ${bigger.rate} pontos`;
+}
+
+// Retornar a maior nota de cada pessoa estudante
+const returnHighestScoreAllStudents = () => {
+  let score = [];
+  const studentsFullNames = returnNameAndLastname(students);
+  students.forEach((item) => {
+    score.push(checkTheHighestScore(students, item.nome, item.sobrenome));
+  })
+  return score;
+}
+console.log(returnHighestScoreAllStudents());
